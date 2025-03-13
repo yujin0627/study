45abfc30c744050b6f3c61a2c6085728b50a0760
Choi-Sang-Gyun <fortis913913@gmail.com>
2025-03-13 18:15:29 +0900

    sg0313commit

---
+++ sanggyun/mar/Mar13_01.java
@@ -0,0 +1,15 @@
+package Mar;
+
+public class Mar13_01 {
+//	실수 flo가 매개 변수로 주어질 때, flo의 정수 
+//	부분을 return하도록 solution 함수를 완성해주세요.
+	
+	public int solution(double flo) {
+        int answer = (int)flo;
+        
+        
+        return answer;
+    }
+	
+	
+}
